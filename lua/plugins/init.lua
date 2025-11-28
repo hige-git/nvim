445566e5ffe01f784bc82eea54886618ccfe0d54
HuberMiguel <hubermiguelandres@gmail.com>
2025-11-28 01:48:10 +0100

    Removed no needed init

--- lua/plugins/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-  require("plugins.cmp"),
-  require("plugins.lsp"),
-  require("plugins.telescope"),
-  require("plugins.neotree"),
-  require("plugins.colorscheme"),
-  require("plugins.term"),
-  require("plugins.markdown"),
-}
