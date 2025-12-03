743f88824d14d2327ded685f5c6c89500b475cc5
hige-git <hubermiguelandres@gmail.com>
2025-12-03 11:13:51 +0100

    corrected go lsp for mason

--- lua/plugins/lsp/lang/gopls.lua
+++ lua/plugins/lsp/lang/gopls.lua
@@ -1,12 +1,5 @@
-local kernel = vim.loop.os_uname.sysname()
-local go_cmd
-
-if kernel == "Darwin" then
-  go_cmd = { "/opt/homebrew/bin/gopls" }
-else
-  go_cmd = { os.getenv("HOME") .. "/go/bin/gopls" }
-end
 
 return {
-  cmd = go_cmd
+  cmd = { "gopls" },
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
 }
