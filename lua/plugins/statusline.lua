927a8cf4c81327f056f1ecfd36f9b7df43edcd33
Miguel Huber <hubermiguelandres@gmail.com>
2025-12-08 03:07:02 +0100

    Small improvement and cleanup

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,25 +1,13 @@
 return {
  {
    "nvim-lualine/lualine.nvim",
-   enabled = true,
    dependencies = {
      "nvim-tree/nvim-web-devicons",
    },
    opts = {
      options = {
-       theme = 'gruvbox_dark',
-       section_separators = '',
-       component_separators = '',
-       icons_enabled = true,
+       theme = 'gruvbox_material',
     },
-    sections = {
-      lualine_a = { 'mode' },
-      lualine_b = { 'branch', 'diff', 'diagnostics' },
-      lualine_c = { 'filename' },
-      lualine_x = { 'filetype' },
-      lualine_y = { 'progress' },
-      lualine_z = { 'location' },
-    }
    }
  },
  {
