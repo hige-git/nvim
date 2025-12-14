dbaf8b16f47f77d44f722186478366c1b29a9166
HuberMiguel <hubermiguelandres@gmail.com>
2025-12-14 10:12:38 +0100

    Integrated vscode-diff to git config in nvim

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,11 +1,17 @@
 return {
-  "NeogitOrg/neogit",
-  dependencies = { 
-    "nvim-lua/plenary.nvim",
-    "sindrets/diffview.nvim",
-  },
-  opts = {
-    disable_signs = false,
-  }
+ {
+    "NeogitOrg/neogit",
+    dependencies = { 
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+    },
+    opts = {
+      disable_signs = false,
+    }
+ },
+ {
+    "esmuellert/vscode-diff.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+ }
 }
 
