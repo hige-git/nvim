0f604da7134f061878320a2d02d6ae38cc1b07cd
HuberMiguel <hubermiguelandres@gmail.com>
2026-03-11 01:38:56 +0100

    cleanup mason-lspconfig

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -24,33 +24,5 @@ return {
       automatic_installation = true,
       ensure_installed = install.lang_serves,
     },
-
-    config = function(_, plugin_opts)
-      local mason_lspconfig = require("mason-lspconfig")
-
-      mason_lspconfig.setup({
-        ensure_installed = plugin_opts.ensure_installed,
-        automatic_installation = plugin_opts.automatic_installation,
-
-        handlers = {
-          function(server)
-            local server_opts = {
-              on_attach = require("plugins.lsp.handlers").on_attach,
-              capabilities = require("plugins.lsp.handlers").capabilities,
-            }
-
-            local ok, conf = pcall(require, "plugins.lsp.lang." .. server)
-            if ok and type(conf) == "table" then
-              server_opts = vim.tbl_deep_extend("force", server_opts, conf)
-            end
-
-            print(server_opts)
-            vim.lsp.config(server, server_opts)
-            vim.lsp.enable(server)
-            -- vim.lsp.config[server].setup(server_opts)
-          end,
-        },
-      })
-    end,
   },
 }
